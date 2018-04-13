271325d406fe6dfa7e8fbe77b4e34f11d8aaa6b1
Arthur Lacoste <arthur.lacoste2@gmail.com>
2018-04-13 03:03:04 -0700

    Include game words in the trimmer.
    The set of game words is somehow not a subset of the vocabulary (allowed clues), but we need embeddings for them too.
    
    Actually we still have a few (<10) missing embeddings:
    1) for compound words, like "new york", "ice cream". These are probably in the model under "new_york", "ice_cream", so it should be a relatively easy fix.
    2) Some not to crazy words that are somehow not covered by the 3M words model, like "olympus", "aztecs"

--- word2vec_trimmer.py
+++ word2vec_trimmer.py
@@ -1,10 +1,15 @@
 import gensim
 import numpy
 
-def read_vocabulary():
-    with open('./data/vocab.txt') as f:
+def words_from_file(filename):
+    with open(filename) as f:
         return [line.strip() for line in f]
-        
+
+def get_game_vocabulary():
+    vocab = words_from_file('./data/vocab.txt')
+    game_words = words_from_file('./data/game_words.txt')
+    return list(set(vocab) | set(game_words)), game_words
+    
 def get_trimmed_model(game_vocab, model_vocab, model_vectors):
     trimmed_model_vocab = {}
     trimmed_model_vectors = numpy.zeros(shape=(len(game_vocab), model_vectors.shape[1]))
@@ -13,7 +18,7 @@ def get_trimmed_model(game_vocab, model_vocab, model_vectors):
     model_vocab_words_list = list(model_vocab)
     for word in game_vocab:
         if ((index + num_words_missing_from_model) % 1000 == 0):
-            print('Kept ' + str(index) + ' words out of ' + str(index + num_words_missing_from_model))
+            print('Kept %d words out of %d' % (index, index + num_words_missing_from_model))
         if (not word in model_vocab):
             num_words_missing_from_model = num_words_missing_from_model + 1
             continue
@@ -21,7 +26,7 @@ def get_trimmed_model(game_vocab, model_vocab, model_vectors):
         trimmed_model_vocab[word].index = index
         trimmed_model_vectors[index,:] = model_vectors[model_vocab_words_list.index(word),:]
         index = index + 1
-    print(str(num_words_missing_from_model) + ' words from the game vocabulary could not be found in the model')
+    print('%d words from the game vocabulary could not be found in the model' % num_words_missing_from_model)
     
     # If we didn't manage to keep as many words as we'd like, reshape trimmed_model_vectors.
     if (index < trimmed_model_vectors.shape[0]):
@@ -30,18 +35,23 @@ def get_trimmed_model(game_vocab, model_vocab, model_vectors):
 
 if __name__ == '__main__':
     print('Reading game vocabulary...')
-    game_vocab = read_vocabulary()
-    print('Done reading game vocabulary! [' + str(len(game_vocab)) + ' words]')
+    game_vocab, game_words = get_game_vocabulary()
+    print('Done reading game vocabulary! [%d words]' % len(game_vocab))
 
     # On windows, you will need to be running 64-bit python to be able to load that large a model.
     print("Loading word2vec model...")
     model = gensim.models.KeyedVectors.load_word2vec_format('./data/GoogleNews-vectors-negative300.bin', binary=True)
-    print('Done loading word2vec model! [' + str(len(model.vocab)) + ' words]')
+    print('Done loading word2vec model! [%d words]' % len(model.vocab))
+    
+    # List unknown game words.
+    unknown_game_words = list(set(game_words).difference(set(model.vocab)))
+    if (len(unknown_game_words) > 0):
+        print('The following %d game words are unknown to the model: %s' % (len(unknown_game_words), str.join(', ', unknown_game_words)))
     
     print('Trimming model down to game vocabulary...')
     trimmed_model = model
     trimmed_model.vocab, trimmed_model.vectors = get_trimmed_model(game_vocab, model.vocab,  model.vectors)
-    print('Done trimming model! [' + str(len(trimmed_model.vocab)) + ' words]')
+    print('Done trimming model! [%d words]' % len(trimmed_model.vocab)
     
     print('Saving trimmed model...')
     trimmed_model.save_word2vec_format('./data/GoogleNews-trimmed-word2vec-negative300.bin', binary=True)
